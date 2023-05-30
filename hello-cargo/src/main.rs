648a71f6edf8a15a9b5780ffc43a90f013903278
TaironeAlves <115471921+taironeAlves@users.noreply.github.com>
2023-05-30 18:30:57 -0300

    300523_1830

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,3 +1,16 @@
+// Objetos ou Structs
+struct Student {
+    name: String,
+    level: u8,
+    remote: bool,
+}
+
+//Struct somente com tipos de dados
+struct Grades(char, char, char, char, f32);
+
+//Struct tipo unidade
+struct Unit;
+
 fn main() {
     // Call prinln! whit three arguments: a string, a value, a value;
     println!(
@@ -34,5 +47,38 @@ fn main() {
 
     // Teste tipagem bolean
     let is_bigger = 1 > 4;
-    print!("Is 1 > 4? {}", is_bigger);
+    println!("Is 1 > 4? {}", is_bigger);
+
+    //tuplas (array)
+    let tuple_e = ('E', 5i32, true);
+
+    //Exemplo de uso da tupla
+    println!(
+        "Is '{}' the {}th letter of the alphabet? {}",
+        tuple_e.0, tuple_e.1, tuple_e.2
+    );
+
+    // Exemplo de uso do Structs
+    let user_1 = Student {
+        name: String::from("Constance Sharma"),
+        remote: true,
+        level: 2,
+    };
+
+    let user_2 = Student {
+        name: String::from("Tairone Alves"),
+        level: 5,
+        remote: false,
+    };
+    let mark_1 = Grades('A', 'A', 'A', 'A', 3.75);
+    let mark_2 = Grades('B', 'A', 'A', 'C', 3.25);
+
+    println!(
+        "{}, level {}. Remote: {}. Grades: {}, {}, {}, {}. Average: {}",
+        user_1.name, user_1.level, user_1.remote, mark_1.0, mark_1.1, mark_1.2, mark_1.3, mark_1.4
+    );
+    println!(
+        "{}, level {}. Remote: {}. Grades: {}, {}, {}, {}. Average: {}",
+        user_2.name, user_2.level, user_2.remote, mark_2.0, mark_2.1, mark_2.2, mark_2.3, mark_2.4
+    );
 }
