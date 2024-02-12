f051d1ce5cbfeb3ce9519b554a212a03dfddbeba
Tairone <tairone.alves@gmail.com>
2024-02-11 23:06:27 -0300

    110224_2306

---
+++ livro-rust/enums/src/main.rs
@@ -0,0 +1,75 @@
+/* enum VersaoIp {
+    V4(String),
+    V6(u32),
+}
+
+struct EnderecoIp {
+    versao: VersaoIp,
+    endereco: String,
+}
+
+fn main() {
+    // let local = EnderecoIp {
+    //     versao: VersaoIp::V4,
+    //     endereco: String::from("127.0.0.1"),
+    // };
+
+    // let loopback = EnderecoIp {
+    //     versao: VersaoIp::V6,
+    //     endereco: String::from("::1"),
+    // };
+
+    // Outra forma de representar as enums sem o uso de Struct
+    let outro_local = VersaoIp::V4(String::from("127.0.0.1"));
+    // let outro_loopback = VersaoIp::V6(String::from("::1"));
+    let outro_loopback = VersaoIp::V6(1);
+}
+
+fn rotear(versao_ip: &VersaoIp) {}
+ */
+
+// Outra forma de usar enums. As enums assim como structs podem receber métodos com impl
+/*
+enum Mensagem {
+    Sair,
+    Mover { x: i32, y: i32 },
+    Escrever(String),
+    MudarCor(i32, i32, i32),
+}
+
+ */
+
+// Usando a enum Option<t>
+// O enum Option ele faz parte do Padrao do Rust, como o rust não permite valores null
+// Option retorna um Some() quando tem algum valor ou None quando não tem nada.
+/* fn main (){
+   let algum_numero = Some(5);
+   let algum_texto = Some("um texto");
+
+   let numero_ausente:Option<i32> = None;
+} */
+
+// Utlizando o match para capturar o valor de Option<T>
+
+enum Moeda {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn valor_em_cents(moeda: Moeda) -> u32 {
+    match moeda {
+        Moeda::Penny => 1,
+        Moeda::Dime => 10,
+        Moeda::Nickel => 5,
+        Moeda::Quarter => 25,
+    }
+}
+
+fn main() {
+    let resp = valor_em_cents(Moeda::Quarter);
+    println!("{}", resp);
+}
+
+// parei na 135
--- livro-rust/retangulo/src/main.rs
+++ livro-rust/retangulo/src/main.rs
@@ -28,6 +28,7 @@ fn area(dimession: (u32, u32)) -> u32 {
 }
  */
 
+/*
  struct Rectangle {
     length: u32,
     width: u32,
@@ -45,4 +46,121 @@ fn area(dimession: (u32, u32)) -> u32 {
     rectangle.length * rectangle.width
 
  }
-// parei na linha 109
+ */
+
+/* // Imprimir com structs para debugar
+
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {length:50, width:30};
+
+    println!("rect1 is {:#?}", rect1);
+}
+ */
+
+// Utilizando structs com metodos traits
+
+/*
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        length: 50,
+        width: 30,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels",
+        rect1.area()
+    );
+}
+ */
+
+//  Colocando um retangulo dentro de outro
+/*
+#[derive(Debug)]
+struct Rectagle {
+    length: u32,
+    width: u32,
+}
+
+trait Area {
+    fn area(&self) -> u32;
+}
+
+trait Hold {
+    fn can_hold(&self, other: &Rectagle) -> bool;
+}
+
+impl Area for Rectagle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+}
+
+impl Hold for Rectagle {
+    fn can_hold(&self, other: &Rectagle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
+
+fn main() {
+    let rect1 = Rectagle {
+        length: 50,
+        width: 30,
+    };
+    let rect2 = Rectagle {
+        length: 40,
+        width: 10,
+    };
+    let rect3 = Rectagle {
+        length: 45,
+        width: 60,
+    };
+
+    println!("Can rect 1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect 1 hold rect3? {}", rect1.can_hold(&rect3));
+}
+
+ */
+
+// Funções associadas, usando com o impl. String::from é um exemplo de funções associadas
+// Essas funções não precisam do parametro &self
+// O &self serve para usar as propriedades da Struct, enum...
+
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+struct Quadrado {}
+
+impl Quadrado {
+    fn square(size: u32) -> Rectangle {
+        return Rectangle {
+            length: size,
+            width: size,
+        };
+    }
+}
+
+fn main() {
+    let sq = Quadrado::square(3);
+    println!("{:?}", sq.length);
+}
