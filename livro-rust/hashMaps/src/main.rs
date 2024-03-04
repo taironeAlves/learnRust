02b88988cc4d5e07d6c8bb3af6f933326f8eb360
Tairone <tairone.alves@gmail.com>
2024-03-03 22:44:50 -0300

    030324_2244

--- livro-rust/hashMaps/src/main.rs
+++ livro-rust/hashMaps/src/main.rs
@@ -1,4 +1,6 @@
-use std::collections::HashMap;
+// Definindo HashMaps
+
+/* use std::collections::HashMap;
 
 fn main() {
 let mut scores = HashMap::new();
@@ -9,3 +11,41 @@ let mut scores = HashMap::new();
 
 }
 
+ */
+
+// Acessando valores do hashMap
+/* 
+use std::collections::HashMap;
+
+fn main() {
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let score = scores.get("Blue");
+
+    println!("{:?}", scores["Blue"]);
+}
+ */
+
+use std::collections::HashMap;
+
+// Interando com um um HashMap. HashMap a key pode ser do tipo inteiro ou String e value pode ser de qualquer tipo
+// porem todos os keys e values tem que ser do mesmo tipo que foi definido primeiro.
+
+fn main() {
+    let mut times = HashMap::new();
+
+    times.insert(String::from("tairone"),10);
+    times.insert(String::from("noeli"), 12);
+
+    // println!("{:?}", times.get("tairones"));
+
+    for(key, value) in &times {
+        println!("{}: {}", key, value);
+    }
+}
+
+// Parei na pagina 175
